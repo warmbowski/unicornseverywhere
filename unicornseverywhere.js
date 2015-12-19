50b5f4166eaecd391eb467166344fab78a783247
Paul Laskowski <pelaskowski@gmail.com>
2015-12-18 22:58:35 -0800

    finish off basic functionality of baiting unicorns into corrals

--- unicornseverywhere.js
+++ unicornseverywhere.js
@@ -3,30 +3,37 @@ Corrals = new Mongo.Collection('corrals');
 
 
 if (Meteor.isClient) {
-  Session
-  // counter starts at 0
-  var food = ['steak', 'ice cream', 'rainbows', 'candy', 'none']
-  Session.setDefault('food', );
+
+  var baitList = ['steak', 'ice cream', 'rainbows', 'candy', 'none']
+  var baitsUsed = [];
+  var baitsUsedDep = new Tracker.Dependency;
+
+  var getBaitsUsed = function () {
+    baitsUsedDep.depend();
+    return baitsUsed;
+  };
+
+  var setBaitsUsed = function (newValue) {
+    if (newValue !== baitsUsed) baitsUsedDep.changed();
+    baitsUsed = newValue;
+  };
 
   Template.corrals.helpers({
     corrals: function() {
+      var baits = []
       var myCorrals = Corrals.find().map(function(corral) {
         corral.location = corral.location.toUpperCase() + ' CORRAL';
+        corral.caught = Unicorns.find({favFood: corral.bait}).fetch();
+        baits.push(corral.bait);
+        setBaitsUsed(baits);
         return corral;
       });
       return myCorrals;
     },
 
     unicorns: function() {
-      var corral = Corrals.findOne();
-      console.log(Unicorns.find({location: corral.location}).fetch());
-      return Unicorns.find({location: corral.location}).fetch();
-    }
-  });
-
-  Template.unicorns.helpers({
-    unicorns: function() {
-      return Unicorns.find().fetch();
+      console.log(getBaitsUsed());
+      return Unicorns.find({favFood: {$nin: getBaitsUsed()}});
     }
   });
 
@@ -35,7 +42,7 @@ if (Meteor.isClient) {
       //add bait
 
       var id = evt.target.id;
-      var bait = _.sample(food);
+      var bait = _.sample(baitList);
 
       Corrals.update(id, {$set: {bait: bait}});
     }
